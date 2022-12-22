a1d746bfc9e0ad5d3acee71d82630b947d3aa710
Anuj <anujk.581@gmail.com>
2022-12-21 23:58:23 -0500

    updated file duplicate element

--- Arrays/DuplicateElement.java
+++ Arrays/DuplicateElement.java
@@ -2,8 +2,18 @@
 //
 public class DuplicateElement {
 
-    public static int findDuplicate(){
+    public static int findDuplicate(int[] ar) {
 
+        int i, j;
+
+        for (i = 0; i < ar.length - 1; i++) {
+            if (ar[i] == ar[i + 1]) {
+                j = i + 1;
+                while (ar[j] == ar[i]) j++;
+                System.out.println(ar[i] + "appears " + (j - i) + "times");
+                i = j - 1;
+            }
+        }
         return 0;
     }
 
