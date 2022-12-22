d60e43744d4769c0ec505b1285ba466f26179ed6
Anuj <anujk.581@gmail.com>
2022-12-22 00:13:04 -0500

    fixed implementation

--- Arrays/DuplicateElement.java
+++ Arrays/DuplicateElement.java
@@ -1,8 +1,8 @@
 //class to find if any duplicates in SORTED array also find out the time it occurs.
-//
+//[1, 2, 3, 4, 4, 5, 6, 7, 7, 7, 8]  -> 4*2    7*3
 public class DuplicateElement {
 
-    public static int findDuplicate(int[] ar) {
+    public static void findDuplicate(int[] ar) {
 
         int i, j;
 
@@ -10,15 +10,19 @@ public class DuplicateElement {
             if (ar[i] == ar[i + 1]) {
                 j = i + 1;
                 while (ar[j] == ar[i]) j++;
-                System.out.println(ar[i] + "appears " + (j - i) + "times");
-                i = j - 1;
+                System.out.println(ar[i] + " appears -> " + (j - i) + " times");
+                i = j - 1; //bring i back to position next to repeated element.
             }
         }
-        return 0;
+
     }
 
 
     public static void main(String[] args) {
+
+
+        int [] a1 = {1, 2, 3, 4, 4, 5, 6, 7, 7, 7, 8};
+        findDuplicate(a1);
 
     }
 }
