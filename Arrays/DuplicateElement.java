b673fed13f2ae4eeb5c280784f6a4a838a20cfe2
Anuj <anujk.581@gmail.com>
2022-12-21 23:49:38 -0500

    duplicate element in sorted array

---
+++ Arrays/DuplicateElement.java
@@ -0,0 +1,14 @@
+//class to find if any duplicates in SORTED array also find out the time it occurs.
+//
+public class DuplicateElement {
+
+    public static int findDuplicate(){
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
